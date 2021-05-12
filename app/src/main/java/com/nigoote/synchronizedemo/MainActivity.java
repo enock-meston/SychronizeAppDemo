95ed5cbdbf07eb503d157cfdc974c06e33898e97
Enock <ndagijimanaenock11@gmail.com>
2021-05-12 19:32:48 +0200

    GUI

--- app/src/main/java/com/nigoote/synchronizedemo/MainActivity.java
+++ app/src/main/java/com/nigoote/synchronizedemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.nigoote.synchronizedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+        //this is the menu method
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu,menu);
+        return true;
     }
 }
